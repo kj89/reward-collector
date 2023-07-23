3c81744c38417f6620cf0a487f4bf147d854be3e
Kristaps Jahimovics <kristaps.jahimovics@gmail.com>
2023-07-23 19:40:17 +0300

    Add gas adjustment

--- main.py
+++ main.py
@@ -73,6 +73,7 @@ def withdraw_rewards(daemon: str,
                   f"--fees {fees} " \
                   f"--output json " \
                   f"--gas auto " \
+                  f"--gas-adjustment 1.3" \
                   f"--yes"
         logger.debug(f"Withdraw rewards: {command}")
         result = run(command, shell=True, capture_output=True, text=True)
@@ -119,6 +120,7 @@ def transfer_to_osmosis(daemon: str,
                   f"--fees {fees} " \
                   f"--output json " \
                   f"--gas auto " \
+                  f"--gas-adjustment 1.3" \
                   f"--yes"
         logger.debug(f"Transfer to Osmosis: {command}")
         logger.info(f"Transfer {withdrawal_amount} {denom} to {osmosis_address}...")
